b8d542a2f3b268409acf68e6e784e0ccf1c9e0d1
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-12-05 18:43:43 -0300

    feat: add function to handler player input

--- main.cpp
+++ main.cpp
@@ -5,14 +5,27 @@
 const int RES_X = 1920;
 const int RES_Y = 1080;
 
+void HandleInput(Player *player)
+{
+    if (IsKeyPressed(KEY_W))
+    {
+        player->shape->y -= 100;
+    }
+
+    if (IsKeyPressed(KEY_S))
+    {
+        player->shape->y += 100;
+    }
+}
+
 int main()
 {
     InitWindow(RES_X, RES_Y, "Best Pong Ever");
-
-    Player *player = new Player(100, 100, (RES_X / 2) - 200, (RES_Y / 2) + 200, BLACK);
+    Player *player = new Player(50, 150, (RES_X / 2) - 500, (RES_Y / 2) + 200, BLACK);
     
     while(!WindowShouldClose())
     {
+        HandleInput(player);
         BeginDrawing();
             ClearBackground(RAYWHITE);
             player->Render();
