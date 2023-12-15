ce9f64ebab60719855de710d4d88450f3c076dce
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-12-15 12:34:22 -0300

    feat: add Opponent instatiation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "ball.h"
+#include "opponent.h"
 #include "player.h"
 #include "raylib.h"
 #include <iostream>
@@ -19,33 +20,31 @@ void HandleInput(Player *player) {
   }
 }
 
-bool GameObjectOutOfBounds(Vector2 position) {
-  return position.x <= 0;
-}
+bool GameObjectOutOfBounds(Vector2 position) { return position.x <= 0; }
 void ResetGameObjectPosition(Vector2 &position) {
   position.x = RES_X / 2.0 + 10;
 }
 
+// TODO: Bounce on the top and bottom of screen 
+
 int main() {
   InitWindow(RES_X, RES_Y, "Best Pong Ever");
-  Player *player = new Player(RECT_WIDTH, RECT_HEIGHT, (RES_X / 2) - 500,
+  Player *player = new Player(RECT_WIDTH, RECT_HEIGHT, (RES_X / 2) - 800,
                               (RES_Y / 2) + 100, BLACK);
 
   SetTargetFPS(60);
-
-  Rectangle *opponent = new Rectangle();
-  opponent->x = (RES_X / 2.0) + 500;
-  opponent->y = (RES_Y / 2.0) + 100;
-  opponent->width = RECT_WIDTH;
-  opponent->height = RECT_HEIGHT;
 
   Ball *ball = new Ball(RES_X / 2, RES_Y / 2, 20, BLACK);
   ball->speed.x = 4.0f;
   ball->speed.y = 2.0f;
   ball->lastCollision = NULL;
 
+  Opponent *opponent =
+      new Opponent({(RES_X / 2.0) + 800, (RES_Y / 2.0) + 100}, {2.0f, 4.0f},
+                   RECT_WIDTH, RECT_HEIGHT, BLACK);
+
   while (!WindowShouldClose()) {
-    // Update 
+    // Update
     HandleInput(player);
     Collision collision = ball->DetectCollision(player->shape);
 
@@ -56,8 +55,10 @@ int main() {
     if (!collision.collide) {
       ball->Move();
     } else {
-      Collision *lastCollision = new Collision(); 
-      lastCollision->point = { collision.point.x, collision.point.y };
+      bool right = ball->position.x >= GetScreenWidth() / 2.0;
+      Collision *lastCollision = new Collision();
+      lastCollision->point = {collision.point.x, collision.point.y};
+      lastCollision->right = collision.right;
       ball->lastCollision = lastCollision;
 
       ball->Move();
@@ -66,9 +67,10 @@ int main() {
 
     // Draw
     BeginDrawing();
-      ClearBackground(RAYWHITE);
-      player->Render();
-      ball->Render();
+    ClearBackground(RAYWHITE);
+    player->Render();
+    opponent->Render();
+    ball->Render();
     EndDrawing();
     //
   }
